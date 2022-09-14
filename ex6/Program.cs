78ec90a36ef3757184acee17befe39a33891f4d0
Cherentcov_Danil <ablazov64@gmail.com>
2022-09-14 11:11:40 +0400

    ex6

---
+++ ex6/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int f = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (f > max) max = f;
+
+Console.Write ("max = ");
+Console.WriteLine (max);
