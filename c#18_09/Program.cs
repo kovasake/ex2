c2b356441604c82a0a7f90e1206419fb7e71f2b3
Cherentcov_Danil <ablazov64@gmail.com>
2022-09-19 15:09:48 +0400

    c#18_09

---
+++ c#18_09/Program.cs
@@ -0,0 +1,24 @@
+﻿// Console.Clear ();
+// Console.WriteLine (" Введите первое число: ");
+// int a = int.Parse (Console.ReadLine ());
+
+// Console.WriteLine (" Введите второе число: ");
+// int b = int.Parse (Console.ReadLine ());
+
+// Console.WriteLine (" Введите третье число: ");
+// int c = int.Parse (Console.ReadLine ());
+
+// int numberMax = int.MinValue;
+// numberMax = numberMax > a ? numberMax : a;
+// numberMax = numberMax > b ? numberMax : b;
+// numberMax = numberMax > c ? numberMax : c;
+// Console.WriteLine ("Максимальное число " - numberMax);
+
+// Console.Clear ();
+// Console.WriteLine (" Введите первое число: ");
+// int number = int.Parse (Console.ReadLine ());
+
+// if (number % 2 ==0) 
+// Console.WriteLine ($"Число {number} - четное");
+// else 
+// Console.WriteLine ($"Число {number} - нечетное");
